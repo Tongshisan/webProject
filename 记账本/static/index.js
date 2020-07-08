d089fabe10984738b65f40e7501e3859ca63a285
李智 <lizhi@danale.com>
2020-07-08 21:30:01 +0800

    feat: 记账本

---
+++ 记账本/static/index.js
@@ -0,0 +1,12 @@
+const balance = document.getElementById('balance')
+const income = document.getElementById('income')
+const expense = document.getElementById('expense')
+const main = document.getElementById('main')
+
+function init() {
+    balance.innerHTML = 0.00
+    income.innerHTML = 0.00
+    expense.innerHTML = 0.00
+}
+
+init()
