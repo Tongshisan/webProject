606738d2a3c51938bfc4ce5ced86054a13ec61b7
李智 <lizhi@danale.com>
2020-07-10 21:21:14 +0800

    feat: 记忆卡

--- 记忆卡片/static/index.js
+++ 记忆卡片/static/index.js
@@ -1,4 +1,31 @@
 const test = document.getElementsByClassName('card')
-test[0].addEventListener('click', function() {
-    test[0].classList.toggle('show-answer')
+const btn = document.getElementById('addCardBtn')
+const cardIndexSpan = document.getElementById('cardIndexSpan')
+const cardCountSpan = document.getElementById('cardCountSpan')
+const next = document.getElementById('next')
+const pre = document.getElementById('pre')
+let index = 1
+let cardCount = 2
+next.addEventListener('click', function () {
+    if (index < cardCount) {
+        index++
+        test[index - 2].className = 'card left'
+        test[index-1].className = 'card active'
+        updatePages()
+    }
 })
+pre.addEventListener('click', function () {
+    if (index > 1) {
+        index--
+        test[index-1].className = 'card active'
+        test[index].className = 'card right'
+        updatePages()
+    }
+})
+
+function updatePages() {
+    cardIndexSpan.innerHTML = index
+    cardCountSpan.innerHTML = cardCount
+}
+
+updatePages()
